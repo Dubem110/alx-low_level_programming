6e03aa1c0f66b7694f8169e936032042b4e4dc32
Dubem110 <dubemanozie.ac@gmail.com>
2023-02-19 08:11:58 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible combinations of singe numbers.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		putchar(x + '0');
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

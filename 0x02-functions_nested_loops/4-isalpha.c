bbd23580cab6957930395a5ab87522dfb77a6330
Dubem110 <dubemanozie.ac@gmail.com>
2023-02-22 05:11:48 -0800

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
- * Rerurn: 1 for alphabetic character or 0 for anything else
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
@@ -11,9 +11,7 @@ int _isalpha(int c)
 {
 
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
-
-	return (1);
-	}
-	return (0);
+		return (1);
+	else
+		return (0);
 }
